32653f4d5d6254e7f2f9843f4fa02f1c6d6d98e2
George Aoyagi <aoyagigeorge@gmail.com>
2020-04-10 22:47:22 -0700

    refactored and fixed strings.pyy

--- strings.py
+++ strings.py
@@ -5,6 +5,21 @@ def contains(text, pattern):
     assert isinstance(text, str), 'text is not a string: {}'.format(text)
     assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
     # TODO: Implement contains here (iteratively and/or recursively)
+    num = 0     #index to keep track of which letter in pattern you're comparing to you
+    if len(pattern) == 0:
+        return True 
+    for letter in text:
+        #if they arent equal and you're part of the way into comapring to the pattern
+        if num>0 and letter != pattern[num]:
+            num = 0
+        # check if your current letter matches a letter in pattern
+        if letter == pattern[num]:
+            num+=1
+        #if you're finished with the pattern then finish ahead of time
+        if num > len(pattern)-1:
+            return True
+        
+    return False
 
 
 def find_index(text, pattern):
@@ -13,6 +28,30 @@ def find_index(text, pattern):
     assert isinstance(text, str), 'text is not a string: {}'.format(text)
     assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
     # TODO: Implement find_index here (iteratively and/or recursively)
+    num = 0     #index to keep track of which letter in pattern you're comparing to you
+    index = 0
+    if len(pattern) == 0:
+        return index
+    for x in range(len(text)):
+        print(num)
+        print(text[x])
+        print(pattern[num])
+        print(index)
+        #if they arent equal and you're part of the way into comapring to the pattern
+        if num>0 and text[x] != pattern[num]:
+            num = 0         #reset the num counter
+            index = 0       #reset the index counter
+        # check if your current letter matches a letter in pattern
+        if text[x] == pattern[num]:
+            if num == 0:
+                index = x
+            num+=1
+        #if you're finished with the pattern then finish ahead of time
+        if num > len(pattern)-1:
+            return index
+    
+    return None
+
 
 
 def find_all_indexes(text, pattern):
@@ -21,6 +60,64 @@ def find_all_indexes(text, pattern):
     assert isinstance(text, str), 'text is not a string: {}'.format(text)
     assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
     # TODO: Implement find_all_indexes here (iteratively and/or recursively)
+    index = []
+    for t in range(len(text)):
+        #if your pattern is an empty string
+        if len(pattern) == 0:
+            index.append(t)
+            continue
+        print("iteration: ", t, text[t])
+        # check if your current letter matches a letter in pattern
+        if text[t] == pattern[0]:
+            index.append(t)     #add index of the first character
+            x = t               #temp var to iterate using the current t
+            #loop over the rest of the word to see if
+            for y in range(len(pattern)):
+                #if not equal it means it isnt a full pattern
+                print(text[x])
+                print(pattern[y])
+                if text[x] != pattern[y]:
+                    print("deleted")
+                    del index[len(index)-1]     #delete the index from indexes because the pattern is broken
+                    break
+                #is equal but reached end of text and pattern hasnt ended yet
+                elif x==len(text)-1 and y<len(pattern)-1:
+                    print("deleted bc end of text")
+                    del index[len(index)-1]     #delete the index from indexes because the pattern is broken
+                    break
+                if x<len(text)-1:
+                    x+=1
+            print("end of pattern search")
+                   
+
+            
+        #     if p < len(pattern)-1:
+        #         p+=1
+        #         # print(pattern[p],"1")
+        #         #check if you reached the end of the loop in the middle of finding a pattern:
+        #         if t == len(text)-1:  
+        #             del index[len(index)-1]     #delete the index from indexes because the pattern is broken
+        #     #if you've finished finding 1 pattern
+        #     else:
+        #         p = 0
+        #         # print(pattern[p],"2")
+        #     t+=1    #go to the next character in text
+        #     # print(text[t], "b")
+        # else:
+        #     #if they arent equal in the middle of a pattern
+        #     if p>0:
+        #         p = 0                 #reset the num counter
+        #         # print(pattern[p], "3")
+        #         del index[len(index)-1] #delete the index from indexes because the pattern is broken
+        #     if text[t] == pattern[p]:
+        #         # print("stuck here")
+        #         index.append(t)
+        #         t+=1
+        #         # print(text[t], "c")
+        #     else:
+        #         t+=1
+    return index
+    
 
 
 def test_string_algorithms(text, pattern):
@@ -52,5 +149,7 @@ def main():
         print("find_all_indexes('abra cadabra', 'abra') => [0, 8]")
 
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
+print(find_all_indexes('ababc', 'abc'))
+#assert find_all_indexes('abc', 'a') == [0]  # multiple letters are harder
